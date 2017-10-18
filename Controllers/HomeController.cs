52b59d6eb6d1154c354dd7aa1b5d26321ac456f3
Raj <rajxeon@gmail.com>
2017-10-18 02:01:13 -0700

    18/10/2017
    18/10/2017

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Microsoft.AspNet.Identity;
 
 namespace EzUtility.Controllers
 {
+
+    
+
     public class HomeController : Controller
     {
+        public EzUtilityEntities dbContext=new EzUtilityEntities();
+        
+
         public ActionResult Index()
         {
             return View();
@@ -15,8 +24,19 @@ namespace EzUtility.Controllers
 
         public ActionResult Ez()
         {
+            
             ViewData["Message"] = "Welcome to EzPatch";
             return View();
+        }
+
+        
+
+        public string getServiceLine() {            
+            var context = new EzUtilityEntities(); 
+            var query =(from c in context.ServiceLines select c);
+            var json = JsonConvert.SerializeObject(query);
+            return json+ User.Identity.GetUserId();
+            
         }
 
     }
