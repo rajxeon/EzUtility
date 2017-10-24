f7d46a1ff42f3790701c119803209d601e8519b0
Raj <rajxeon@gmail.com>
2017-10-24 08:14:29 -0700

    10/24/2017
    Installed kbs done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,10 +8,15 @@ using Newtonsoft.Json;
 using Microsoft.AspNet.Identity;
 using System.Diagnostics;
 using EzUtility.Models;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Text;
 
 namespace EzUtility.Controllers
 {
 
+    
     
 
     public class HomeController : Controller
@@ -25,10 +30,63 @@ namespace EzUtility.Controllers
         }
 
         public ActionResult Ez()
-        {
-            
-            ViewData["Message"] = "Welcome to EzPatch";
+        {   ViewData["Message"] = "Welcome to EzPatch";
             return View();
+        }
+
+        [HttpPost]
+        public string RunPS(string filename, string first_param = "", string second_param = "", string third_param = "") {
+
+            RunspaceConfiguration runspaceConfiguration = RunspaceConfiguration.Create();
+
+            // create Powershell runspace
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+
+            // open it
+            runspace.Open();
+
+            // create a pipeline and feed it the script text
+            Pipeline pipeline = runspace.CreatePipeline();
+
+            //string command_path = Server.MapPath("~") + @"App_Data\PS\getLog.ps1";
+            string command_path = Server.MapPath("~") + @"PSScripts\"+ filename + ".ps1";
+            Command myCommand = new Command(command_path);
+
+            //-----------------------------PARAMS--------------------------------
+
+            if (first_param.Length > 0) {
+                CommandParameter testParam = new CommandParameter(first_param.Split('>')[0], first_param.Split('>')[1]);
+                myCommand.Parameters.Add(testParam);
+            }
+            if (second_param.Length > 0)
+            {
+                CommandParameter testParam1 = new CommandParameter(second_param.Split('>')[0], second_param.Split('>')[1]);
+                myCommand.Parameters.Add(testParam1);
+            }
+            if (third_param.Length > 0)
+            {
+                CommandParameter testParam2 = new CommandParameter(third_param.Split('>')[0], third_param.Split('>')[1]);
+                myCommand.Parameters.Add(testParam2);
+            }
+
+            //-------------------------------------------------------------------
+
+            pipeline.Commands.Add(myCommand);
+
+            Collection<PSObject> results = pipeline.Invoke();
+
+            runspace.Close();
+
+            // convert the script result into a single string
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+
+            return stringBuilder.ToString();
+
+             
         }
 
         
---
+++ PSScripts/test.ps1
@@ -0,0 +1,5 @@
+param([string]$param1='1',
+[string]$param2='2',
+[string]$param3='3'
+)
+"Hi,test successfull<br>"+$param1+"<br>"+$param2+"<br>"+$param3
